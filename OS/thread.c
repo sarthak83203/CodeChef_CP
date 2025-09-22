d33f0ced89133d4ab4a50448bc7ad08c01a0d6f6
sarthak <sarthak.shinde2006@gmail.com>
2025-09-22 22:31:06 +0530

    This is thread

---
+++ OS/thread.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// Shared number
+int num;
+
+// Function to check prime
+void* check_prime(void* arg) {
+    int n = num;
+    int isPrime = 1;
+
+    if (n <= 1) isPrime = 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("Thread 1: %d is a prime number.\n", n);
+    else
+        printf("Thread 1: %d is not a prime number.\n", n);
+
+    pthread_exit(NULL);
+}
+
+// Function to calculate factorial
+void* calc_factorial(void* arg) {
+    int n = num;
+
+    if (n > 20) {
+        printf("Thread 2: Number too large for factorial calculation.\n");
+    } else {
+        unsigned long long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        printf("Thread 2: Factorial of %d is %llu\n", n, fact);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    pthread_t t1, t2;
+
+    // Create threads
+    pthread_create(&t1, NULL, check_prime, NULL);
+    pthread_create(&t2, NULL, calc_factorial, NULL);
+
+    // Wait for both threads to finish
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Main: Both threads have finished.\n");
+
+    return 0;
+}
