6648410183c6e15ea63c25961d91686b7247b159
sarthak <sarthak.shinde2006@gmail.com>
2025-09-28 15:20:55 +0530

    Delection Code Important for Interview

--- Deletion_code.c
+++ Deletion_code.c
@@ -48,18 +48,50 @@ void * inorder(struct node* root){
 
 }
 
-//Deletion Code......
+
+//Deletion code start...
+struct node* inorderProcedure(struct node *root){
+    //left subtree ka right most child...is an inorder[Procedure//]
+    root=root->left;
+    while(root->right!=NULL){
+        root=root->right;
+    }
+    return root;
+
+}
+
+struct node *deletenode(struct node *root,int value){
+     struct node* inpre;
+    if(root==NULL){
+        return NULL;
+
+    }
+    if(root->left==NULL && root->right==NULL){
+        free(root); //free the meemory location ....
+        return NULL;
+    }
 
 
-
-
+    if(value>root->data){
+        root->right=deletenode(root->right,value);
+    }else if(value<root->data){
+       root->left= deletenode(root->left,value);
+    }
+    //Deletion actual start...
+    else{
+        inpre=inorderProcedure(root);
+        root->data=inpre->data;
+        root->left=deletenode(root->left,inpre->data);
+    }
+    return root; //return those node...
+}
 
 
 int main(){
-    struct node* p=createnode(7); //root 
-    struct node* p1=createnode(2); //children
-    struct node* p2=createnode(1); //children
-    struct node* p3=createnode(0);//children
+    struct node* p=createnode(5); //root 
+    struct node* p1=createnode(3); //children
+    struct node* p2=createnode(6); //children
+    struct node* p3=createnode(1);//children
     struct node* p4=createnode(4);//children
   
     
@@ -70,6 +102,9 @@ int main(){
       preorder(p);
       printf("\n");
       postorder(p);
+      printf("\n");
+      inorder(p);
+      deletenode(p,3);
       printf("\n");
       inorder(p);
    
